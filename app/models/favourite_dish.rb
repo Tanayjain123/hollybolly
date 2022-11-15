abfc99445d6dcacae6be2144dc46cdec55e3ff9a
“tanay <tanay@preciousinfosystem.com>
2022-11-15 18:39:10 +0530

    created favouritedish  model

---
+++ app/models/favourite_dish.rb
@@ -0,0 +1,4 @@
+class FavouriteDish < ApplicationRecord
+  belongs_to :user
+  belongs_to :product
+end
---
+++ test/models/favourite_dish_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class FavouriteDishTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
